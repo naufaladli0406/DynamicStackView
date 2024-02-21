c0f1aa9fb05c35bfe64c0034f56f7afd3a4df03b
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2024-02-21 14:55:49 +0700

    Initial Commit

---
+++ DynamicStackView/DynamicStackViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicStackViewApp.swift
+//  DynamicStackView
+//
+//  Created by Naufal Adli on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DynamicStackViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
